aa3639d1f6a14fc877dce8553e93230dd14924cb
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-08-23 22:56:10 +0530

    -init

---
+++ lib/data/expense_data.dart
@@ -0,0 +1,16 @@
+class ExpenseData{
+
+  // list of all expenses
+
+  // get expense list
+
+  //add new expense
+
+  //delete expense
+
+  // get weekday from date time
+
+  //get the date for the start of the week (sunday)
+
+  //
+}
